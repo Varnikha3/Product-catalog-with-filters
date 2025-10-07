eda0c4855222aa3d2840d9d33aa69c62da8f6e66
Varnikha3 <vkvarnikha@gmail.com>
2025-10-07 20:29:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterItems(category) {
+  const items = document.querySelectorAll('.item');
+  items.forEach(item => {
+    if (category === 'all' || item.dataset.category === category) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
